9bd2eb80f8b91067c8a9faf1f8238f45df655ecc
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-09-24 10:43:25 +0200

    Création class BankAccount, attributs et getter setter

---
+++ src/BankAccount.java
@@ -0,0 +1,23 @@
+public class BankAccount {
+
+    private String account_number;
+    private double balance;
+
+    public BankAccount(String account_number, double balance) {
+        this.account_number = account_number;
+        this.balance = 0;
+    }
+
+    public String getAccount_number() {
+        return account_number;
+    }
+
+    public void setAccount_number(String account_number) {
+        this.account_number = account_number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+}
