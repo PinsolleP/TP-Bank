9f8c87585b94b97f3fa27b72b9f67b640485f96e
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-09-24 10:42:51 +0200

    Ajout des getters et setter

--- src/Person.java
+++ src/Person.java
@@ -10,5 +10,27 @@ public class Person {
         this.last_name = last_name;
     }
 
+    public int getId_person() {
+        return id_person;
+    }
 
+    public void setId_person(int id_person) {
+        this.id_person = id_person;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
 }
