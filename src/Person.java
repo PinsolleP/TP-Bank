ac11b1052c705c9d5cc3e32c7bf9417a5874d56a
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-09-24 10:38:19 +0200

    Création class Person et attributs

---
+++ src/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    private int id_person;
+    private String first_name;
+    private String last_name;
+
+    public Person(int id_person, String first_name, String last_name) {
+        this.id_person = id_person;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+
+}
